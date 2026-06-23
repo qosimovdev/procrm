0fc03824a6d5d7ed52ed5113b7a705cd7fcaa18b
ibrohim3 <ibrohim.2128382@gmail.com>
2026-06-23 11:51:07 +0500

    add vercel.json

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: 'http://localhost:5577/api',
-    // baseURL: 'http://localhost:5577/api',
-    baseURL: 'https://procrm-backend.onrender.com/api',
+    baseURL: 'http://localhost:5577/api',
+    // baseURL: 'https://procrm-backend.onrender.com/api',
     headers: {
         "Content-Type": "application/json",
     }
